ba04791d81040c10c1f82fcb6dc0ac01d74f4be7
gabrielmumo <gabrielmumo@gmail.com>
2014-01-20 13:37:19 -0500

    Tercer commit

--- PruebaEnero20/src/Test.java
+++ PruebaEnero20/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		try {
 			String str = "pruebadeuncoomit";
-			System.out.println(str.length());
+			System.out.println("Tamagno" + str.length());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
