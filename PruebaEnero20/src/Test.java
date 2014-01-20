afda715e37758d7723faf57203884df0487adb55
gabrielmumo <gabrielmumo@gmail.com>
2014-01-20 13:29:42 -0500

    Segundo commit

---
+++ PruebaEnero20/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		try {
+			String str = "pruebadeuncoomit";
+			System.out.println(str.length());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
